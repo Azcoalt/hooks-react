b58c596a7f6d20d2e8686402c52c0df70aa8ab59
David Torres <davidtorres_work@outlook.com>
2023-07-28 19:05:35 -0600

    end basic hook

--- src/main.jsx
+++ src/main.jsx
@@ -10,10 +10,12 @@ import './index.css';
 //import { FocusScreen } from './04-useRef/FocusScreen';
 //import { Layaut } from './05-useLayoutEffect/Layaut';
 //import { Memorize } from './06-memos/Memorize';
-import { MemoHook } from './06-memos/MemoHook';
+//import { MemoHook } from './06-memos/MemoHook';
+//import { CallBackHook } from './06-memos/CallBackHook';
+import { Padre } from './07-tarea-memo/Padre';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //{<React.StrictMode>}
-    < MemoHook />
+    < Padre />
   //{</React.StrictMode>}
 )
